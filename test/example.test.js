615753b6427f81b2b11c92ab8b6042f099916bfc
Maciek Blim <m.blim@binarapps.com>
2016-02-25 15:15:57 +0100

    basic dev tools configuration

---
+++ test/example.test.js
@@ -0,0 +1,8 @@
+import {expect} from 'chai';
+
+describe('Example test', () => {
+    it('of example #method', () => {
+        const foo = 'foo';
+        expect(foo).to.not.equal('bar');
+    });
+});
